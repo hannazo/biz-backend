044d980f98c1574e5400dfa5833ac8e33ad50500
hannazo <106128732+hannazo@users.noreply.github.com>
2023-03-26 17:30:13 -0400

    added product model

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2), // 10 digits precision, 2 digits scale
+      allowNull: false,
+      validate: {
+        isDecimal: true, // validates that the value is a decimal
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true, // validates that the value is numeric
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      reference: {
+        model: 'category',
+        key: 'id',
+      }
+    },
   },
   {
     sequelize,
